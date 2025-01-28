c1a3bf42caa7d6e034e52d43f04ed5d3818013ef
Yahim0 <yahim0@proton.me>
2025-01-28 13:57:22 -0300

    added vanillaPlusMode

--- src/Config.h
+++ src/Config.h
@@ -3,10 +3,19 @@ namespace SensibleTempering {
     public:
         static void LoadConfig() noexcept;
 
-        inline static bool additiveFallback{};
+        inline static bool vanillaPlusMode{};
 
+        inline static long heavyArmorBonus;
+        inline static long heavyArmorChestBonus;
+        inline static long lightArmorBonus;
+        inline static long lightArmorChestBonus;
+        inline static long oneHandedBonus;
+        inline static long twoHandedBonus;
+        inline static long bowBonus;
+        inline static long crossbowBonus;
+
+        inline static bool vanillaPlusFallback{};
         inline static float percentPerTierArmor{};
-
         inline static float percentPerTierWeapon{};
     };
 }  // namespace betteralttab
